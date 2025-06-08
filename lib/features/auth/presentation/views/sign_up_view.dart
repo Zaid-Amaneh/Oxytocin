9c484db73f48da293ba14a1f5f15b7f373607120
Zaid Amanah <amanahzaid627@gmail.com>
2025-06-08 17:42:46 +0300

    Create sign up view file

---
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:oxytocin/features/auth/presentation/widget/sign_up_view_body.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SignUpViewBody());
+  }
+}
