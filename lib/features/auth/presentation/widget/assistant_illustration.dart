654d2cc4d802146e50ff59154a2dfb475120a392
Zaid Amanah <amanahzaid627@gmail.com>
2025-06-13 19:23:01 +0300

    fix some issues

--- lib/features/auth/presentation/widget/assistant_illustration.dart
+++ lib/features/auth/presentation/widget/assistant_illustration.dart
@@ -11,59 +11,21 @@ class AssistantIllustration extends StatelessWidget {
     final size = MediaQuery.sizeOf(context);
     final width = size.width;
     final height = size.height;
+    List<Widget> imageList = <Widget>[
+      SvgPicture.asset(Assets.imagesAssistantLeftHand),
+      SvgPicture.asset(Assets.imagesAssistantRightHand),
+    ];
     return Positioned(
       height: height * 0.47,
       left: width * 0.08,
       right: width * 0.08,
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 2500),
-        transitionBuilder: (child, animation) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(3.0, -1.0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-        child: form
-            ? SvgPicture.asset(
-                Helper.isArabic()
-                    ? Assets.imagesAssistantRightHand
-                    : Assets.imagesAssistantLeftHand,
-              )
-            : SvgPicture.asset(
-                Helper.isArabic()
-                    ? Assets.imagesAssistantLeftHand
-                    : Assets.imagesAssistantRightHand,
-              ),
-      ),
-      // AnimatedSizeAndFade(
-      //   fadeDuration: const Duration(milliseconds: 10000),
-      //   fadeInCurve: Easing.legacy,
-      //   fadeOutCurve: Easing.legacy,
-      //   // firstChild: SvgPicture.asset(Assets.imagesAssistantsignin),
-      //   // secondChild:
-      //   // duration: const Duration(milliseconds: 2500),
-      //   sizeCurve: Curves.bounceIn,
-      //   // firstChild: SvgPicture.asset(Assets.imagesAssistantsignin),
-      //   // secondChild:
-      //   // duration: const Duration(milliseconds: 2500),
-      //   child: form
-      //       ? SvgPicture.asset(Assets.imagesAssistantsignin)
-      //       : SvgPicture.asset(Assets.imagesAssistantsignup),
-      // ),
-      // child: AnimatedSizeAndFade(
-      //   firstChild: SvgPicture.asset(Assets.imagesAssistantsignin),
-      //   secondChild: SvgPicture.asset(Assets.imagesAssistantsignup),
-      //   duration: const Duration(milliseconds: 2500),
-      //   crossFadeState: form
-      //       ? CrossFadeState.showFirst
-      //       : CrossFadeState.showSecond,
-      //   firstCurve: Curves.fastOutSlowIn,
-      //   secondCurve: Curves.slowMiddle,
-      //   sizeCurve: Curves.fastEaseInToSlowEaseOut,
-      // ),
+      child: form
+          ? Helper.isArabic()
+                ? imageList[1]
+                : imageList[0]
+          : Helper.isArabic()
+          ? imageList[0]
+          : imageList[1],
     );
   }
 }
--- lib/features/auth/presentation/widget/auth_form.dart
+++ lib/features/auth/presentation/widget/auth_form.dart
@@ -2,29 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:oxytocin/features/auth/presentation/widget/sign_in_form.dart';
 import 'package:oxytocin/features/auth/presentation/widget/sign_up_form.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key, required this.form});
   final bool form;
 
   @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+  bool? _previousForm; // Track previous state
+
+  final List<Widget> formList = const [
+    SignInForm(key: ValueKey('sign_in')),
+    SignUpForm(key: ValueKey('sign_up')),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+    _updateAnimation();
+    _controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant AuthForm oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.form != oldWidget.form) {
+      _previousForm = oldWidget.form;
+      _controller.reset();
+      _controller.forward();
+    }
+  }
+
+  void _updateAnimation() {
+    _offsetAnimation = Tween<Offset>(
+      begin: _calculateBeginOffset(),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutQuad));
+  }
+
+  Offset _calculateBeginOffset() {
+    if (_previousForm == null) return Offset.zero;
+
+    return Offset(widget.form ? -2.0 : 2.0, 2.0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-    final height = size.height;
+    _updateAnimation(); // Ensure animation uses latest values
     return Positioned(
-      top: height * 0.3,
+      top: MediaQuery.sizeOf(context).height * 0.3,
       bottom: 0,
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 450),
-        transitionBuilder: (child, animation) {
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: Offset(form ? -2.0 : 2.0, 2.0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          );
-        },
-        child: form ? const SignInForm() : const SignUpForm(),
+      child: SlideTransition(
+        position: _offsetAnimation,
+        child: widget.form ? formList[0] : formList[1],
       ),
     );
   }
